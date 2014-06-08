005ef08aa842894c3e8e1b8ea8366fa8faf5ffa1
Renato Fabbri <renato.fabbri@gmail.com>
2014-06-08 02:12:47 -0300

    feito cliente minimo junto ao server

--- aaServer.py
+++ aaServer.py
@@ -1,9 +1,16 @@
-import os, pymongo, datetime
+import os, pymongo, datetime, string
 from flask import Flask, request
 
 app = Flask(__name__)
 
-client=pymongo.MongoClient("mongodb://labmacambira:macambira00@ds031948.mongolab.com:31948/aaserver")
+
+@app.route('/minimumClient/')
+def minimumClient():
+    client=pymongo.MongoClient("mongodb://labmacambira:macambira00@ds031948.mongolab.com:31948/aaserver")
+    shouts=client.aaserver.shouts.find({},{"_id":0})
+    shouts=string.join([str(ss) for ss in shouts],"<br />")
+    print shouts
+    return shouts
 
 @app.route('/shout/')
 def shout():
@@ -14,6 +21,7 @@ def shout():
     shout=request.args.get("shout","")
     now=datetime.datetime.now()
     print nick, shout, now
+    client=pymongo.MongoClient("mongodb://labmacambira:macambira00@ds031948.mongolab.com:31948/aaserver")
     client.aaserver.shouts.insert({"time":now,"nick":nick,"shout":shout})
     return 'Hello World!'
 
