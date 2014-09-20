802331854a51d27b6d6ceae80766a7ff9ca91058
Renato Fabbri <renato.fabbri@gmail.com>
2014-09-20 19:27:50 -0300

    adicionado script que faz a ontologiaa

---
+++ rdf/fazOntologiaa.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+import rdflib as r
+import time
+T=time.time()
+
+aa = r.Namespace("http://purl.org/socialparticipation/aa/")
+rdf = r.namespace.RDF
+foaf = r.namespace.FOAF
+owl = r.namespace.OWL
+dc=r.namespace.DC
+dct=r.namespace.DCTERMS
+dcty=r.Namespace("http://purl.org/dc/dcmitype/")
+gndo=r.Namespace("http://d-nb.info/standards/elementset/gnd#")
+sc=r.Namespace("http://schema.org/")
+g.namespace_manager.bind("ops", "http://purl.org/socialparticipation/ops#")    
+
+g = r.Graph()
+# faz ontologia:
+# 1) info sobre esta ontologia
+ouri=aa.ontologiaa+".owl"
+g.add((ouri,rdf.type,owl.Ontology))
+g.add((ouri,dct.title,"Ontologiaa"))
+g.add((ouri,owl.versionInfo,"0.01a"))
+g.add((ouri,dct.description,"Ontologia enxuta do AA para conectar o namespace da ontologia com outros namespaces"))
+
+# 2) adicionadas classes pai das classes do aa
+g.add((aa.User,    rdfs.subClassOf, foaf.Person))
+g.add((aa.User,    rdfs.subClassOf, dc.Agent))
+g.add((aa.User,    rdfs.subClassOf, ops.Participant))
+g.add((aa.Shout,   rdfs.subClassOf, foaf.Document))
+g.add((aa.Shout,   rdfs.subClassOf, sioc.Port))
+g.add((aa.Session, rdfs.subClassOf, dcty.Collection))
+g.add((aa.Session, rdfs.subClassOf, dcty.Event))
+
+# 3) declaradas propriedades funcionais
+
+# 4) adicionadas propriedades pai das propriedades
+g.add((aa.checker, rdfs.subPropertyOf, gndo.revisor))
+g.add((aa.session, rdfs.subPropertyOf, sc.collection))
+g.add((aa.user, rdfs.subPropertyOf, foaf.maker))
+
+
+g.add((aa.checkMessage, rdfs.subPropertyOf, sc.text))
+g.add((aa.shoutMessage, rdfs.subPropertyOf, sc.text))
+g.add((aa.screencast, rdfs.subPropertyOf, sc.video))
+
+g.add((aa.created, owl.equivalentProperty, dct.created))
+g.add((aa.nick, owl.equivalentProperty, foaf.nick))
+g.add((aa.email, owl.equivalentProperty, foaf.created))
+
+# ficaram sem: score
+# 5) especificadas características das funções
+g.add((aa.checker, rdf.type, owl.FunctionalProperty))
+g.add((aa.checker, rdf.type, owl.ObjectProperty))
+g.add((aa.session, rdf.type, owl.FunctionalProperty))
+g.add((aa.session, rdf.type, owl.ObjectProperty))
+g.add((aa.user, rdf.type, owl.FunctionalProperty))
+g.add((aa.user, rdf.type, owl.ObjectProperty))
+
+g.add((aa.checkMessage, rdf.type, owl.FunctionalProperty))
+g.add((aa.checkMessage, rdf.type, owl.DataProperty))
+g.add((aa.shoutMessage, rdf.type, owl.FunctionalProperty))
+g.add((aa.shoutMessage, rdf.type, owl.DataProperty))
+g.add((aa.screencast, rdf.type, owl.FunctionalProperty))
+g.add((aa.screencast, rdf.type, owl.DataProperty))
+
+g.add((aa.created, rdf.type, owl.FunctionalProperty))
+g.add((aa.created, rdf.type, owl.DataProperty))
+g.add((aa.nick, rdf.type, owl.FunctionalProperty))
+g.add((aa.nick, rdf.type, owl.DataProperty))
+g.add((aa.email, rdf.type, owl.FunctionalProperty))
+g.add((aa.email, rdf.type, owl.DataProperty))
+
+# 6) declaradas restricoes existenciais
+# object property somente o aa:user
+b_=r.BNode()
+g.add((aa.Shout, rdfs.subClassOf, b_))
+g.add((b_,rdf.type,owl.Restriction))
+g.add((b_,rdf.onProperty,aa.user))
+g.add((b_,owl.someValuesFrom,aa.User))
+
+# data property
+# são existenciais, shoutMessage, created nos dois casos, nick
+b_=r.BNode()
+g.add((aa.Shout, owl.subClassOf, b_))
+g.add((b_,rdf.type,owl.Restriction))
+g.add((b_,rdf.onProperty,aa.shoutMessage))
+g.add((b_,owl.someValuesFrom,xsd.string))
+
+b_=r.BNode()
+g.add((aa.Shout, owl.subClassOf, b_))
+g.add((b_,rdf.type,owl.Restriction))
+g.add((b_,rdf.onProperty,aa.created))
+g.add((b_,owl.someValuesFrom,xsd.dateTime))
+
+b_=r.BNode()
+g.add((aa.Session, owl.subClassOf, b_))
+g.add((b_,rdf.type,owl.Restriction))
+g.add((b_,rdf.onProperty,aa.created))
+g.add((b_,owl.someValuesFrom,xsd.dateTime))
+
+b_=r.BNode()
+g.add((aa.User, owl.subClassOf, b_))
+g.add((b_,rdf.type,owl.Restriction))
+g.add((b_,rdf.onProperty,aa.nick))
+g.add((b_,owl.someValuesFrom,xsd.string))
