22952fef6e06aeda4af212239be9c9b373d60a2d
Danilo Shiga <daniloshiga@gmail.com>
2015-07-07 07:08:44 -0300

    Pep8 fixes

--- aaServer.py
+++ aaServer.py
@@ -1,10 +1,11 @@
-#-*- coding: utf8 -*-
-import os, pymongo, datetime, string
-from flask import Flask, request, jsonify
-# adicione
-### import urllib
-### urllib.urlretrieve("http://0.0.0.0:5000/shout?nick=%s&shout=%s"%("nickEscolhido",urllib.quote("mensagem formatada com urllib.quote"))
-# em um app python para enviar um shout
+# -*- coding: utf8 -*-
+import pymongo
+import datetime
+from flask import Flask, request, jsonify, render_template
+#  adicione
+#  import urllib
+#  urllib.urlretrieve("http://0.0.0.0:5000/shout?nick=%s&shout=%s"%("nickEscolhido",urllib.quote("mensagem formatada com urllib.quote"))
+#  em um app python para enviar um shout
 
 app = Flask(__name__)
 
@@ -12,30 +13,31 @@ app = Flask(__name__)
 @app.route('/allJson/')
 def allJson():
     client=pymongo.MongoClient("mongodb://labmacambira:macambira00@ds031948.mongolab.com:31948/aaserver")
-    shouts=client.aaserver.shouts.find({},{"_id":0})
-    shouts=[ss for ss in shouts]
+    shouts = client.aaserver.shouts.find({}, {"_id": 0})
+    shouts = [ss for ss in shouts]
     return jsonify(shouts=shouts)
+
 
 @app.route('/minimumClient/')
 def minimumClient():
     client=pymongo.MongoClient("mongodb://labmacambira:macambira00@ds031948.mongolab.com:31948/aaserver")
-    shouts=client.aaserver.shouts.find({},{"_id":0})
-    shouts=string.join([str(ss)[1:-1] for ss in shouts][::-1],"<br />"*2).replace("u'","").replace("'","")
-    print shouts
-    return shouts
+    shouts = client.aaserver.shouts.find({}, {"_id": 0}).sort([(u'_id', -1), ])
+    return render_template('minimum_client.html', shouts=shouts)
+
 
 @app.route('/shout/')
 def shout():
     """Chamado com ?nick=prestoppc&shout=aa bb cc: codando o desenhado."""
     print request.args
-    print request.args.get("tkey","")
-    nick=request.args.get("nick","")
-    shout=request.args.get("shout","")
-    now=datetime.datetime.now()
+    print request.args.get("tkey", "")
+    nick = request.args.get("nick", "")
+    shout = request.args.get("shout", "")
+    now = datetime.datetime.now()
     print nick, shout, now
     client=pymongo.MongoClient("mongodb://labmacambira:macambira00@ds031948.mongolab.com:31948/aaserver")
-    client.aaserver.shouts.insert({"time":now,"nick":nick,"shout":shout})
+    client.aaserver.shouts.insert({"time": now, "nick": nick, "shout": shout})
     return 'Hello World!'
+
 
 @app.route('/')
 def hello():
